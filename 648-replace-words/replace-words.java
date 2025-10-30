8dded5d2b6f72892d25980e44aacfc23ed10299d
Tarunsharma45 <145276584+Tarunkumar0089@users.noreply.github.com>
2025-10-30 18:54:15 +0530

    Time: 694 ms (23.69%) | Memory: 55.9 MB (24.72%) - LeetSync

---
+++ 648-replace-words/replace-words.java
@@ -0,0 +1,20 @@
+class Solution {
+    private String findRoot(String word, Set<String> st) {
+        for (int i = 1; i <= word.length(); i++) {
+            String root = word.substring(0, i);
+            if (st.contains(root)) {
+                return root;
+            }
+        }
+        return word;
+    }
+    public String replaceWords(List<String> dictionary, String sentence) {
+        Set<String> st = new HashSet<>(dictionary);
+        StringBuilder result = new StringBuilder();
+        String[] words = sentence.split(" ");
+        for (String word : words) {
+            result.append(findRoot(word, st)).append(" ");
+        }
+        return result.toString().trim();
+    }
+}
