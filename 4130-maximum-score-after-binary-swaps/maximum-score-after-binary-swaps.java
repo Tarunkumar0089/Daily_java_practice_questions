f65f1e874f174a4f492e450468fcdc783b9f4e38
Tarunsharma45 <145276584+Tarunkumar0089@users.noreply.github.com>
2026-01-15 14:39:10 +0530

    Time: 154 ms (20.77%) | Memory: 117 MB (62.26%) - LeetSync

---
+++ 4130-maximum-score-after-binary-swaps/maximum-score-after-binary-swaps.java
@@ -0,0 +1,15 @@
+class Solution {
+    public long maximumScore(int[] nums, String s) {
+        int n=nums.length;
+        PriorityQueue<Integer> pq= new PriorityQueue<>(Collections.reverseOrder());
+        long ans=0;
+        for(int i=0;i<nums.length;i++){
+            pq.add(nums[i]);
+            if(s.charAt(i)=='1'){
+                ans+= pq.poll();
+            }
+        }
+        return ans;
+            
+    }
+}
