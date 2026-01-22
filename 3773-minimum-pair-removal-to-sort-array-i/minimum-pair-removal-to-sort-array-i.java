461042b0527f5c0bc6b8feb1d9c7e7e3f18b2078
Tarunsharma45 <145276584+Tarunkumar0089@users.noreply.github.com>
2026-01-22 11:57:21 +0530

    Time: 3 ms (66.06%) | Memory: 44.6 MB (46.87%) - LeetSync

--- 3773-minimum-pair-removal-to-sort-array-i/minimum-pair-removal-to-sort-array-i.java
+++ 3773-minimum-pair-removal-to-sort-array-i/minimum-pair-removal-to-sort-array-i.java
@@ -2,24 +2,23 @@ class Solution {
     public int minimumPairRemoval(int[] nums) {
         List<Integer> list = new ArrayList<>();
         for (int x : nums) list.add(x);
-        int operations = 0;
+        int count = 0;
         while (!isNonDecreasing(list)) {
-            int minSum = Integer.MAX_VALUE;
-            int index = 0;
+            int min = Integer.MAX_VALUE;
+            int idx = 0;
             for (int i = 0; i < list.size() - 1; i++) {
                 int sum = list.get(i) + list.get(i + 1);
-                if (sum < minSum) {
-                    minSum = sum;
-                    index = i;
+                if (sum < min) {
+                    min = sum;
+                    idx = i;
                 }
             }
-            list.remove(index);      
-            list.remove(index);      
-            list.add(index, minSum); 
-
-            operations++;
+            list.remove(idx);      
+            list.remove(idx);      
+            list.add(idx,min); 
+            count++;
         }
-        return operations;
+        return count;
     }
     public boolean isNonDecreasing(List<Integer> list) {
         for (int i = 1; i < list.size(); i++) {
