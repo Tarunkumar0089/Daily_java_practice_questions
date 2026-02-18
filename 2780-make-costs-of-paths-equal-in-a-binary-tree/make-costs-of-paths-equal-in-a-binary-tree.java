8b032ea5f786c812e8e21e0a0a997e668be81f90
Tarunsharma45 <145276584+Tarunkumar0089@users.noreply.github.com>
2026-02-18 11:43:24 +0530

    Time: 9 ms (21.15%) | Memory: 67.6 MB (15.38%) - LeetSync

--- 2780-make-costs-of-paths-equal-in-a-binary-tree/make-costs-of-paths-equal-in-a-binary-tree.java
+++ 2780-make-costs-of-paths-equal-in-a-binary-tree/make-costs-of-paths-equal-in-a-binary-tree.java
@@ -1,23 +1,17 @@
 class Solution {
-    int[]dp;
+    int ans=0;
     public int minIncrements(int n, int[] cost) {
-        dp=new int[n+1];
-        int req=maxCost(cost,1);
-        return sol(n,cost,1,req,dp);
+     sol(n,0,cost);
+     return ans;
+        
     }
-    public int sol(int n,int[]cost,int i,int req,int[]dp){
-        int left=2*i;
-        int right=2*i+1;
-        if(left>=n && right>=n){
-            return req-cost[i-1];
-        }
-        int self=req-dp[i-1];
-        return self+ sol(n,cost,2*i,req-cost[i-1]-self,dp)+ sol(n,cost,2*i+1,req-cost[i-1]-self,dp);
-    }
-    public int maxCost(int[]cost,int i){
-        if(i>cost.length){
+    public int sol(int n,int i,int[]cost){
+        if(i>=n){
             return 0;
         }
-        return dp[i-1]=cost[i-1]+Math.max(maxCost(cost,2*i),maxCost(cost,2*i+1));
+        int left=sol(n,2*i+1,cost);
+        int right=sol(n,2*i+2,cost);
+         ans+=Math.abs(left-right);
+        return  cost[i] +Math.max(left,right);
     }
 }
