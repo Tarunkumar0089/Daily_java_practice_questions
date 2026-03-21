b64282b68611b1c5f499803a9e6f0b49e7f7ba75
Tarunsharma45 <145276584+Tarunkumar0089@users.noreply.github.com>
2026-03-21 11:18:19 +0530

    Time: 0 ms (100.00%) | Memory: 47.5 MB (57.55%) - LeetSync

--- 3973-flip-square-submatrix-vertically/flip-square-submatrix-vertically.java
+++ 3973-flip-square-submatrix-vertically/flip-square-submatrix-vertically.java
@@ -1,12 +1,12 @@
 class Solution {
     public int[][] reverseSubmatrix(int[][] grid, int x, int y, int k) {
-        int n= grid.length;
-        int m= grid[0].length;
-        for(int i=x;i<x+k/2 && i<n;i++){
-            for(int j=y;j<y+k && j<m;j++){
-                int temp= grid[i][j];
-                grid[i][j]= grid[x+k-1-(i-x)][j];
-                grid[x+k-1-i+x][j]=temp;
+        int n=grid.length;
+        int m=grid[0].length;
+        for(int i=0;i<k/2;i++){
+            for(int j=0;j<k;j++){
+                int temp=grid[x+i][y+j];
+                grid[x+i][y+j] =grid[x+k-1-i][y+j];
+                grid[x+k-1-i][y+j] = temp;
             }
         }
         return grid;
