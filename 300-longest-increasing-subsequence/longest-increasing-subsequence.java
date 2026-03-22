d7b0bcffd2dd36c5cb3df1ccd1649a86d79dca5f
Tarunsharma45 <145276584+Tarunkumar0089@users.noreply.github.com>
2026-03-22 18:34:07 +0530

    Time: 37 ms (73.36%) | Memory: 45.9 MB (94.58%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,18 +1,33 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-         int[][]dp=new int[nums.length][nums.length+1];
-      for(int[]r:dp)   Arrays.fill(r,-1);
-           return sol(0,-1,nums,dp);
-    }
-    public int sol(int i,int prev,int[]nums,int[][]dp){
-      if(i==nums.length) return 0;
-      if(dp[i][prev+1]!=-1) return dp[i][prev+1];
+    //      int[][]dp=new int[nums.length][nums.length+1];
+    //   for(int[]r:dp)   Arrays.fill(r,-1);
+    //        return sol(0,-1,nums,dp);
+        int n = nums.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp,1);  
+        int ans =1;
+        for (int i=0; i<n; i++) {
+            for (int j=0;j<i; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i],1+dp[j]);
+                }
+            }
+            ans = Math.max(ans,dp[i]);
+        }
 
-      int np=sol(i+1,prev,nums,dp);
-      int p=0;
-      if(prev==-1 || nums[i]>nums[prev]){
-        p=1+sol(i+1,i,nums,dp);
-      }
-      return dp[i][prev+1]=Math.max(p,np);
+        return ans;
+
     }
+    // public int sol(int i,int prev,int[]nums,int[][]dp){
+    //   if(i==nums.length) return 0;
+    //   if(dp[i][prev+1]!=-1) return dp[i][prev+1];
+
+    //   int np=sol(i+1,prev,nums,dp);
+    //   int p=0;
+    //   if(prev==-1 || nums[i]>nums[prev]){
+    //     p=1+sol(i+1,i,nums,dp);
+    //   }
+    //   return dp[i][prev+1]=Math.max(p,np);
+    // }
 }
