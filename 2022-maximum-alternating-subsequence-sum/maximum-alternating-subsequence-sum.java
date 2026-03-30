2df91c8179647a53cb8f0590ce6f93c38ed862a4
Tarunsharma45 <145276584+Tarunkumar0089@users.noreply.github.com>
2026-03-30 15:39:46 +0530

    Time: 61 ms (32.08%) | Memory: 120.5 MB (40.96%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.java
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.java
@@ -1,13 +1,29 @@
 class Solution {
     public long maxAlternatingSum(int[] nums) {
         int n=nums.length;
-        long ans=nums[0];
-        for(int i=1;i<n;i++){
-            if(nums[i]>nums[i-1]){
-                ans+=nums[i]-nums[i-1];
-            }
+        // long ans=nums[0];
+        // for(int i=1;i<n;i++){
+        //     if(nums[i]>nums[i-1]){
+        //         ans+=nums[i]-nums[i-1];
+        //     }
+        // }
+        // return ans;
+        long[][]dp=new long[n][2];
+        return solve(nums,0,0,dp);
+    }
+    public static long solve(int[]nums,int i,int s,long[][]dp){
+        if(i==nums.length) return 0;
+
+        if(dp[i][s]!=0) return dp[i][s];
+        long np=solve(nums,i+1,s,dp);
+        long p=0;
+        if(s==0){
+            p=nums[i]+solve(nums,i+1,1,dp);
         }
-        return ans;
+        else{
+            p=-nums[i]+solve(nums,i+1,0,dp);
+        }
+        return dp[i][s]=Math.max(np,p);
 
     }
 }
