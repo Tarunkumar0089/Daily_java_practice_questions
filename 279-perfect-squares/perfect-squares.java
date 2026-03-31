c235744aff4290477a901ca2df47870dc6d105b8
Tarunsharma45 <145276584+Tarunkumar0089@users.noreply.github.com>
2026-03-31 15:22:30 +0530

    Time: 62 ms (34.27%) | Memory: 44.9 MB (23.87%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numSquares(int n) {
+        int[]dp=new int[n+1];
+        return solve(n,dp);
+    }
+
+    public int solve(int n,int[]dp) {
+        if(n == 0) return 0;
+        if(dp[n]!=0) return dp[n];
+        int ans = Integer.MAX_VALUE;
+        for(int i=1;i*i<=n;i++) {
+            ans = Math.min(ans,1 + solve(n-(i*i),dp));
+        }
+        return dp[n]= ans;
+    }
+}
