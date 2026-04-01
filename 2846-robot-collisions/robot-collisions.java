713feee62be338c7aa3bf32690e9683e3e773792
Tarunsharma45 <145276584+Tarunkumar0089@users.noreply.github.com>
2026-04-01 15:48:25 +0530

    Time: 55 ms (41.74%) | Memory: 126.9 MB (35.65%) - LeetSync

--- 2846-robot-collisions/robot-collisions.java
+++ 2846-robot-collisions/robot-collisions.java
@@ -1,49 +1,64 @@
 class Solution {
-    public List<Integer> survivedRobotsHealths(int[] positions, int[] health, String directions) {
-        int n = positions.length;
-        int[][] robots = new int[n][4]; 
-        for (int i = 0; i < n; i++) {
-            robots[i][0] = positions[i];
-            robots[i][1] = health[i];
-            robots[i][2] = directions.charAt(i);
-            robots[i][3] = i;
+    static class Pair {
+        int pos;
+        int health;
+        char dir;
+        int idx;
+        Pair(int pos, int health, char dir, int idx) {
+            this.pos = pos;
+            this.health = health;
+            this.dir = dir;
+            this.idx = idx;
         }
+    }
 
-        Arrays.sort(robots, (a, b) -> a[0] - b[0]);
-        Stack<int[]> st = new Stack<>();
-        for (int[] robot : robots) {
-            if (robot[2] == 'R') {
-                st.push(robot);
+    public List<Integer> survivedRobotsHealths(int[] positions, int[] healths, String directions) {
+        int n = positions.length;
+        List<Pair> list = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            list.add(new Pair(positions[i], healths[i], directions.charAt(i), i));
+        }
+        Collections.sort(list, (a, b) -> Integer.compare(a.pos, b.pos));
+        Stack<Pair> st = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            Pair curr = list.get(i);
+            if (curr.dir == 'R') {
+                st.push(curr);
             } else {
-                while (!st.isEmpty() && st.peek()[2] == 'R') {
-                    int[] top = st.peek();
+                while (!st.isEmpty() && st.peek().dir == 'R') {
 
-                    if (top[1] > robot[1]) {
-                        top[1]--;
-                        robot[1] = 0;
+                    Pair top = st.peek();
+
+                    if (top.health > curr.health) {
+                        top.health--;
+                        curr.health = 0;
                         break;
-                    } else if (top[1] < robot[1]) {
-                        robot[1]--;
+                    } 
+                    else if (top.health < curr.health) {
+                        curr.health--;
                         st.pop();
-                    } else {
+                    } 
+                    else {
                         st.pop();
-                        robot[1] = 0;
+                        curr.health = 0;
                         break;
                     }
                 }
 
-                if (robot[1] > 0) {
-                    st.push(robot);
+                if (curr.health > 0) {
+                    st.push(curr);
                 }
             }
         }
 
-        List<int[]> survivors = new ArrayList<>(st);
-        Collections.sort(survivors, (a, b) -> a[3] - b[3]);
-        List<Integer> result = new ArrayList<>();
-        for (int[] r : survivors) {
-            result.add(r[1]);
+        List<Pair> survivors = new ArrayList<>(st);
+        Collections.sort(survivors, (a, b) -> Integer.compare(a.idx, b.idx));
+        List<Integer> res = new ArrayList<>();
+        for (Pair p : survivors) {
+            res.add(p.health);
         }
-        return result;
+
+        return res;
     }
 }
