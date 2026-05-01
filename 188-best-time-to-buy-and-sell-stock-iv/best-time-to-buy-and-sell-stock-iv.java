dc13e264d8bd1d9cd7346fb08017878c7bd0f06f
Tarunsharma45 <145276584+Tarunkumar0089@users.noreply.github.com>
2026-05-01 21:32:22 +0530

    Time: 9 ms (20.18%) | Memory: 45.1 MB (49.35%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.java
@@ -1,22 +1,16 @@
 class Solution {
-
-    public int solve(int i, int buy, int cap, int[] prices, int[][][] dp){
-        if(i == prices.length || cap == 0)return 0;
-
-        if(dp[i][buy][cap] != -1) return dp[i][buy][cap];
-
+    public int solve(int i, int buy, int cap, int[] prices, int[][][] dp) {
+        if (i == prices.length || cap == 0)return 0;
+        if (dp[i][buy][cap] != -1) return dp[i][buy][cap];
         int profit;
-        if(buy == 1){
+        if (buy == 1) {
             profit = Math.max(
-                    -prices[i] + solve(i+1,0,cap,prices,dp),
-                    solve(i+1,1,cap,prices,dp)
-            );
-        }
-        else{
+                    -prices[i] + solve(i + 1, 0, cap, prices, dp),
+                    solve(i + 1, 1, cap, prices, dp));
+        } else {
             profit = Math.max(
-                    prices[i] + solve(i+1,1,cap-1,prices,dp),
-                    solve(i+1,0,cap,prices,dp)
-            );
+                    prices[i] + solve(i + 1, 1, cap - 1, prices, dp),
+                    solve(i + 1, 0, cap, prices, dp));
         }
 
         return dp[i][buy][cap] = profit;
@@ -25,13 +19,13 @@ class Solution {
     public int maxProfit(int k, int[] prices) {
 
         int n = prices.length;
-        int[][][] dp = new int[n][2][k+1];
+        int[][][] dp = new int[n][2][k + 1];
 
-        for(int i=0;i<n;i++)
-            for(int j=0;j<2;j++)
-                for(int c=0;c<=k;c++)
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < 2; j++)
+                for (int c = 0; c <= k; c++)
                     dp[i][j][c] = -1;
 
-        return solve(0,1,k,prices,dp);
+        return solve(0, 1, k, prices, dp);
     }
 }
