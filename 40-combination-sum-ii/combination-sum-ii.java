23e220df73ff6ebaa762388e58b95d9d7f0345f2
Tarunsharma45 <145276584+Tarunkumar0089@users.noreply.github.com>
2026-06-18 21:53:39 +0530

    Time: 5 ms (99.63%) | Memory: 44.9 MB (93.04%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,28 +1,24 @@
 class Solution {
-    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        Arrays.sort(candidates);
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> ll= new ArrayList<>();
-        Solve(candidates,target,0,ans,ll);
+    public List<List<Integer>> combinationSum2(int[] arr, int target) {  
+        Arrays.sort(arr);
+     List<Integer> list=new ArrayList<>();
+        List<List<Integer>> ans=new ArrayList<>();
+        sol(arr, target,list, 0,ans);
         return ans;
-
     }
-    public static void Solve(int [] candidates, int target, int idx, List<List<Integer>> ans,
-    List<Integer> ll){
-        if(target==0){
-            ans.add(new ArrayList<>(ll));
-            return;
-        }
-        if(idx>= candidates.length){
-            return;
-        }
-        if(target>=candidates[idx]){
-            ll.add(candidates[idx]);
-            Solve(candidates, target-candidates[idx],idx+1,ans,ll);
-            ll.remove(ll.size()-1);
-        }
-        while(idx+1<candidates.length && candidates[idx]==candidates[idx+1]) idx++;
-        
-        Solve(candidates,target,idx+1,ans,ll);
+    public static void sol(int[] arr,int target,List<Integer>list,int idx,List<List<Integer>>ans){
+    if(target==0){
+        ans.add(new ArrayList<>(list));
+        return;
     }
+    for(int i=idx;i<arr.length;i++){
+        if (i > idx && arr[i] == arr[i - 1]) continue;
+        if (arr[i] > target) break; 
+        if(target>=arr[i]){
+            list.add(arr[i]);
+            sol(arr,target-arr[i],list,i+1,ans);
+            list.remove(list.size()-1);
+        }
+    }
+}
 }
