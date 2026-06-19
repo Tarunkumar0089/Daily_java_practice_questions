029de05112c6a110fee06d9afd9251949aa100a0
Tarunsharma45 <145276584+Tarunkumar0089@users.noreply.github.com>
2026-06-19 18:04:39 +0530

    Time: 16 ms (28.23%) | Memory: 48.8 MB (80.08%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,19 +1,19 @@
 class Solution {
     class Node{
-        int first;
-        int second;
-        int third;
-        Node(int f,int s,int t){
-            this.first=f;
-            this.second=s;
-            this.third=t;
+        int x;
+        int y;
+        int d;
+        Node(int x,int y,int d){
+            this.x=x;
+            this.y=y;
+            this.d=d;
         }
     }
     public int[][] updateMatrix(int[][] mat) {
         int n=mat.length;
         int m=mat[0].length;
         boolean[][] vis=new boolean[n][m];
-        int [][] dist=new int[n][m];
+        int [][] ans=new int[n][m];
         Queue<Node> q=new LinkedList<>();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
@@ -28,32 +28,31 @@ class Solution {
         }
         while(!q.isEmpty()){
             Node curr = q.poll();
-            int x = curr.first;
-            int y = curr.second;
-            int d = curr.third;
-            dist[x][y]=d;
-
-//Left
+            int x = curr.x;
+            int y = curr.y;
+            int d = curr.d;
+            ans[x][y]=d;
+        
           if(y-1>=0 && !vis[x][y-1]){
             vis[x][y-1]=true;
             q.add(new Node(x,y-1,d+1));
           }
-//Right
+
            if(y+1<m && !vis[x][y+1]){
             vis[x][y+1]=true;
             q.add(new Node(x,y+1,d+1));
           }
-//Up
+
            if(x-1>=0 && !vis[x-1][y]){
             vis[x-1][y]=true;
             q.add(new Node(x-1,y,d+1));
-          } 
-//Down
+          }
+
           if(x+1<n && !vis[x+1][y]){
             vis[x+1][y]=true;
             q.add(new Node(x+1,y,d+1));
           }
         }
-        return dist;
+        return ans;
     }
 }
