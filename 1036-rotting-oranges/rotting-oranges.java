f9ba5ff0c45346ff0bc7b4ed180dd1d2ce58e80f
Tarunsharma45 <145276584+Tarunkumar0089@users.noreply.github.com>
2026-06-22 16:35:20 +0530

    Time: 2 ms (90.77%) | Memory: 44.3 MB (24.68%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -2,41 +2,50 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
+
         Queue<int[]> q = new LinkedList<>();
         int fresh = 0;
+
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 2) q.add(new int[]{i, j});
-                if (grid[i][j] == 1) fresh++;
+                if (grid[i][j] == 2) {
+                    q.offer(new int[]{i, j});
+                } else if (grid[i][j] == 1) {
+                    fresh++;
+                }
             }
         }
+
         if (fresh == 0) return 0;
+
+        int[][] dir = {
+            {1, 0},   // down
+            {-1, 0},  // up
+            {0, 1},   // right
+            {0, -1}   // left
+        };
+
         int time = -1;
+
         while (!q.isEmpty()) {
             int size = q.size();
             time++;
-            while (size--> 0) {
+
+            while (size-- > 0) {
                 int[] curr = q.poll();
-                int x = curr[0], y = curr[1];
-                if (x + 1 < m && grid[x + 1][y] == 1) {
-                    grid[x + 1][y] = 2;
-                    fresh--;
-                    q.add(new int[]{x + 1, y});
-                }
-                if (x - 1 >= 0 && grid[x - 1][y] == 1) {
-                    grid[x - 1][y] = 2;
-                    fresh--;
-                    q.add(new int[]{x - 1, y});
-                }
-                if (y + 1 < n && grid[x][y + 1] == 1) {
-                    grid[x][y + 1] = 2;
-                    fresh--;
-                    q.add(new int[]{x, y + 1});
-                }
-                if (y - 1 >= 0 && grid[x][y - 1] == 1) {
-                    grid[x][y - 1] = 2;
-                    fresh--;
-                    q.add(new int[]{x, y - 1});
+
+                for (int[] d : dir) {
+                    int ni = curr[0] + d[0];
+                    int nj = curr[1] + d[1];
+                    
+                    if (ni >= 0 && ni < m &&
+                        nj >= 0 && nj < n &&
+                        grid[ni][nj] == 1) {
+
+                        grid[ni][nj] = 2;
+                        fresh--;
+                        q.offer(new int[]{ni, nj});
+                    }
                 }
             }
         }
