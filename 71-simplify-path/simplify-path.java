67b116145e03b3e32434afe8906896f55ece9a3e
Tarunsharma45 <145276584+Tarunkumar0089@users.noreply.github.com>
2026-06-23 16:51:02 +0530

    Time: 8 ms (11.68%) | Memory: 46.2 MB (12.49%) - LeetSync

--- 71-simplify-path/simplify-path.java
+++ 71-simplify-path/simplify-path.java
@@ -1,9 +1,9 @@
 class Solution {
     public String simplifyPath(String path) {
-        String[] arr = path.split("/");
         Stack<String> st = new Stack<>();
-        for (String str : arr) {
-            if (str.equals("") || str.equals(".")) {
+        String[]arr=path.split("/");
+        for (String str:arr) {
+            if (str.equals("")||str.equals(".")) {
                 continue;
             }
             if (!str.equals("..")) {
@@ -14,13 +14,17 @@ class Solution {
                 }
             }
         }
-        if (st.isEmpty()) {
+
+        if(st.isEmpty()){
             return "/";
         }
-        String res = "";
-        for (String s : st) {
-            res += "/" + s;
+
+        String res="";
+        for(String s:st){
+            res+="/"+s;
         }
         return res;
+
+        
     }
 }
