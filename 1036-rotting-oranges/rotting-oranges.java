38cadc5f74719f4f898492677b845601c6ecf2ed
Tarunsharma45 <145276584+Tarunkumar0089@users.noreply.github.com>
2026-06-27 12:44:00 +0530

    Time: 2 ms (90.73%) | Memory: 44.2 MB (55.96%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -2,46 +2,29 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
-
-        Queue<int[]> q = new LinkedList<>();
-        int fresh = 0;
-
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 2) {
-                    q.offer(new int[]{i, j});
-                } else if (grid[i][j] == 1) {
+        Queue<int[]>q=new LinkedList<>();
+        int fresh=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==2){
+                    q.offer(new int[]{i,j});
+                }else if(grid[i][j]==1){
                     fresh++;
                 }
             }
         }
-
         if (fresh == 0) return 0;
-
-        int[][] dir = {
-            {1, 0},   // down
-            {-1, 0},  // up
-            {0, 1},   // right
-            {0, -1}   // left
-        };
-
-        int time = -1;
-
+         int[][]dir={{1,0},{0,1},{-1,0},{0,-1}};
+        int time =-1;
         while (!q.isEmpty()) {
-            int size = q.size();
             time++;
-
-            while (size-- > 0) {
-                int[] curr = q.poll();
-
-                for (int[] d : dir) {
-                    int ni = curr[0] + d[0];
-                    int nj = curr[1] + d[1];
-                    
-                    if (ni >= 0 && ni < m &&
-                        nj >= 0 && nj < n &&
-                        grid[ni][nj] == 1) {
-
+            int size=q.size();
+            while(size-->0){
+                int[]curr=q.poll();
+                for(int []d:dir){
+                    int ni=curr[0]+d[0];
+                    int nj=curr[1]+d[1];
+                     if (ni >= 0 && ni < m && nj >= 0 && nj < n &&grid[ni][nj] == 1) {
                         grid[ni][nj] = 2;
                         fresh--;
                         q.offer(new int[]{ni, nj});
@@ -49,7 +32,6 @@ class Solution {
                 }
             }
         }
-
         return fresh == 0 ? time : -1;
     }
 }
