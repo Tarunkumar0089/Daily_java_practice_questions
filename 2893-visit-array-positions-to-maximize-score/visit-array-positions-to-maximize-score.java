6d69701422d72e42ec679af4db472ab903d3bb7c
Tarunsharma45 <145276584+Tarunkumar0089@users.noreply.github.com>
2026-08-09 08:22:21 +0530

    Time: 61 ms (14.86%) | Memory: 107.6 MB (16.67%) - LeetSync

---
+++ 2893-visit-array-positions-to-maximize-score/visit-array-positions-to-maximize-score.java
@@ -0,0 +1,24 @@
+class Solution {
+    public long maxScore(int[] nums, int x) {
+        int n=nums.length;
+        long[][]dp=new long[n][2];
+        for(long[]d:dp){
+            Arrays.fill(d,-1);
+        }
+        return nums[0]+sol(1,nums[0]%2,nums,dp,n,x);
+    }
+    public long sol(int idx,int p,int[]nums,long[][]dp,int n,int x){
+      if(idx>=n) return 0;
+      if(dp[idx][p]!=-1) return dp[idx][p];
+      int cp=nums[idx]%2;
+      long np=sol(idx+1,p,nums,dp,n,x);
+      long pi;
+      if(cp==p){
+         pi=nums[idx]+sol(idx+1,cp,nums,dp,n,x);
+      }else{
+        pi=nums[idx]-x+sol(idx+1,cp,nums,dp,n,x);
+      }
+
+      return dp[idx][p]=Math.max(pi,np);
+    }
+}
