ce6bd5359f8abc263c56a13e27dc855aaa44c931
Tarunsharma45 <145276584+Tarunkumar0089@users.noreply.github.com>
2026-08-10 08:05:16 +0530

    Time: 7 ms (13.49%) | Memory: 51.4 MB (85.93%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,24 +1,52 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int ans=0;
-        for(int i=0;i<grid.length;i++){
-            for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]=='1'){
-                    Solve(grid,i,j);
-                    ans++;
+        int m = grid.length;
+        int n = grid[0].length;
+        int count = 0;
+        int[][] directions = {
+            {-1, 0},   // up
+            {1, 0},    // down
+            {0, -1},   // left
+            {0, 1}     // right
+        };
+
+        Queue<int[]> queue = new LinkedList<>();
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == '1') {
+
+                    count++;
+                    queue.offer(new int[]{i, j});
+                    grid[i][j] = '0';
+
+                    while (!queue.isEmpty()) {
+
+                        int[] current = queue.poll();
+
+                        int r = current[0];
+                        int c = current[1];
+                          for (int[] dir : directions) {
+
+                            int nr = r + dir[0];
+                            int nc = c + dir[1];
+
+                            // Valid cell and land
+                            if (nr >= 0 && nr < m &&
+                                nc >= 0 && nc < n &&
+                                grid[nr][nc] == '1') {
+
+                                queue.offer(new int[]{nr, nc});
+
+                                // Mark immediately when adding to queue
+                                grid[nr][nc] = '0';
+                            }
+                        }
+                    }
                 }
             }
         }
-        return ans;
-    }
-    public static void Solve(char[][] arr, int i, int j){
-        if(i<0 || i>=arr.length || j<0 || j>=arr[0].length || arr[i][j]!='1'){
-            return;
-        }
-        arr[i][j]='0';
-        Solve(arr,i+1,j);
-        Solve(arr,i-1,j);
-        Solve(arr,i,j+1);
-        Solve(arr,i,j-1);
+
+        return count;
     }
 }
