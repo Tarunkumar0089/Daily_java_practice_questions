5314936efbd05b002fe34d3971f36ba27f1970b2
Tarunsharma45 <145276584+Tarunkumar0089@users.noreply.github.com>
2026-08-15 12:52:00 +0530

    Time: 1 ms (69.10%) | Memory: 43.7 MB (32.51%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -2,7 +2,6 @@ class Solution {
     public int maxProfit(int[] prices) {
         int n = prices.length;
         int[][] dp = new int[n][2];
-
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < 2; j++) {
                     dp[i][j]= -1;
