1f710dc3b728accd3aced80ec4a604f532a69ac7
Tarunsharma45 <145276584+Tarunkumar0089@users.noreply.github.com>
2026-09-21 15:57:10 +0530

    Time: 7 ms (56.51%) | Memory: 46.4 MB (11.64%) - LeetSync

--- 139-word-break/word-break.java
+++ 139-word-break/word-break.java
@@ -1,23 +1,23 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
-        Boolean[] dp = new Boolean[s.length()]; // FIX
-        HashSet<String> set = new HashSet<>(wordDict);
-        int maxLen = 0;
-        for (String word : wordDict) {
-            maxLen = Math.max(maxLen, word.length());
+        Boolean[][]dp=new Boolean[s.length()][s.length()];
+        HashSet<String>set=new HashSet<>();
+        for(String st:wordDict){
+            set.add(st);
         }
-        return sol(s, 0, dp, set, maxLen);
+        return sol(s,set,0,0,dp);
     }
+    public boolean sol(String s,HashSet<String>set,int i,int j,Boolean[][]dp){
+        if(i>=s.length()) return true;
+        if(j>=s.length()) return false;
 
-    public boolean sol(String s, int i, Boolean[] dp, HashSet<String> set, int maxLen) {
-        if (i == s.length()) return true;
-        if (dp[i] != null) return dp[i]; 
-        for (int j = i + 1; j <= s.length() && j - i <= maxLen; j++) {
-            if (set.contains(s.substring(i, j)) && sol(s, j, dp, set, maxLen)) {
-                return dp[i] = true;
-            }
+        if(dp[i][j]!=null) return dp[i][j];
+
+        boolean np=sol(s,set,i,j+1,dp);
+        boolean p=false;
+        if(set.contains(s.substring(i,j+1))){
+            p=sol(s,set,j+1,j+1,dp);
         }
-
-        return dp[i] = false;
+        return dp[i][j]=p||np;
     }
 }
